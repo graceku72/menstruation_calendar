722721d4fe8b3bdaa22563f887d53f86ad1020fd
Grace Ku <gku@unc.edu>
2022-07-11 22:48:26 -0400

    File name updates

--- menstruation_calendar.py
+++ menstruation_calendar.py
@@ -1,5 +1,3 @@
-# Testing for GitHub attempt 3 - 7/11/2022
-
 """I created this menstrual cycle calendar for a couple reasons. 
 For data privacy, I wanted a menstruation calendar for my personal use. 
 Further, while the supplemental options provided in existing menstruation apps can be very useful, I opt for something simpler.
