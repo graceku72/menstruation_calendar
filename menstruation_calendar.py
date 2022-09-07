7d82c7ea063d17a50768d44782cb71828c6798dd
Grace Ku <gku@unc.edu>
2022-09-07 12:48:18 -0400

    Final, 9/7/2022

--- menstruation_calendar.py
+++ menstruation_calendar.py
@@ -1,8 +1,3 @@
-"""I created this menstrual cycle calendar for a couple reasons. 
-For data privacy, I wanted a menstruation calendar for my personal use. 
-Further, while the supplemental options provided in existing menstruation apps can be very useful, I opt for something simpler.
-I could've chosen to use the calendar app on my phone, but this already holds other events. I wanted a calendar with one purpose and one purpose only: to mark the dates of menstruation."""
-
 from tkinter import *
 from tkcalendar import *
 import json
